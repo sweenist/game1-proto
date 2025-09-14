aec4a158d32c913be43548c5ede0ab9ac3557d32
Ryan Sweeney <sween119@hotmail.com>
2025-09-14 16:17:38 -0400

    testing key presses

--- src/tests/gameInput.spec.ts
+++ src/tests/gameInput.spec.ts
@@ -1,13 +1,24 @@
-import { describe, it, expect, vi } from 'vitest';
-import { GameInput, UP } from '../gameInput';
-
-const input = new GameInput();
+import { describe, it, expect } from 'vitest';
+import { DOWN, GameInput, LEFT, RIGHT, UP } from '../gameInput';
 
 describe('gameInput', () => {
-  it('should have a direction of recently pressed button', () => {
-    const event = new KeyboardEvent('keydown, {key: "arrowUp"');
+  const testCases = [
+    { key: 'ArrowUp', expectedDirection: UP },
+    { key: 'KeyW', expectedDirection: UP },
+    { key: 'ArrowDown', expectedDirection: DOWN },
+    { key: 'KeyS', expectedDirection: DOWN },
+    { key: 'ArrowLeft', expectedDirection: LEFT },
+    { key: 'KeyA', expectedDirection: LEFT },
+    { key: 'ArrowRight', expectedDirection: RIGHT },
+    { key: 'KeyD', expectedDirection: RIGHT },
+  ];
+  it.each(testCases)('should have direction $expectedDirection when $key is pressed', ({ key, expectedDirection }) => {
+    const input = new GameInput();
+
+    const event = new KeyboardEvent('keydown', { key });
     document.dispatchEvent(event);
 
-    expect(input.direction).toBe(UP);
+    expect(document).toBeDefined();
+    expect(input.direction).toBe(expectedDirection);
   });
 });
