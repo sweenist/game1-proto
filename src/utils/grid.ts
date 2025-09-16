419c2263a6ab84e293335d8f629832e0d7625803
Ryan Sweeney <sween119@hotmail.com>
2025-09-15 21:42:45 -0400

    Adding walls and a test. Learned about everything is referenc etype

--- src/utils/grid.ts
+++ src/utils/grid.ts
@@ -1,3 +1,11 @@
+import { Vector2 } from "./vector";
+
 export const gridCells = (n: number) => {
   return n * 16;
+}
+
+export const isSpaceFree = (walls: Set<string>, x: number, y: number) => {
+  var checkPosition = new Vector2(x, y);
+
+  return !walls.has(checkPosition.toString());
 }
