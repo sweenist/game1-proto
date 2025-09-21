56e94a4915acb7f0c35381074b0d430779294868
Ryan Sweeney <sween119@hotmail.com>
2025-09-21 09:58:17 -0400

    adding tsdoc comment to vector

--- src/utils/vector.ts
+++ src/utils/vector.ts
@@ -18,4 +18,22 @@ export class Vector2 {
   public static Zero(): Vector2 {
     return new Vector2(0, 0);
   }
+
+  /*
+  Returns whether a comparative vector matches the source vector.
+  @param that a target vector to compare against.
+  @returns true if the target vector components equal components of source vector.
+  */
+  equals(that: Vector2) {
+    return this.x === that.x && this.y === that.y;
+  }
+
+  /*
+  Returns whether a comparative vector rounds to the components of the source vector.
+  @param that a transient vector to compare against.
+  @returns true if the transient vector rounds to the source vector.
+  */
+  prettyClose(that: Vector2) {
+    return Math.round(that.x) === this.x && Math.round(that.y) === this.y;
+  }
 }
