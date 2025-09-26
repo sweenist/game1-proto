ed379f15a0f881c0590dd9f59d19efe06092bfc4
Ryan Sweeney <sween119@hotmail.com>
2025-09-26 06:26:39 -0400

    font rendering with ttf complete

--- src/objects/TextBox/TextBox.ts
+++ src/objects/TextBox/TextBox.ts
@@ -6,6 +6,7 @@ import { Vector2 } from '../../utils/vector';
 export class TextBox extends GameObject {
   content: string;
   backdrop: Sprite;
+  d: number = 0;
 
   constructor() {
     super(new Vector2(36, 112));
@@ -24,7 +25,7 @@ export class TextBox extends GameObject {
   drawImage(ctx: CanvasRenderingContext2D, x: number, y: number): void {
     this.backdrop.drawImage(ctx, x, y);
 
-    ctx.font = '16px fontRetroGaming';
+    ctx.font = '12px fontRetroGaming';
     ctx.textAlign = 'left';
     ctx.textBaseline = 'top';
     ctx.fillStyle = '#eee';
@@ -33,6 +34,26 @@ export class TextBox extends GameObject {
     const LINE_HEIGHT = 20;
     const PADDING_LEFT = 10;
     const PADDING_TOP = 12;
-    ctx.fillText(this.content, x + PADDING_LEFT, y + PADDING_TOP);
+    const SPACE = ' ';
+
+    let words = this.content.split(SPACE);
+    let line = '';
+    for (let i = 0; i < words.length; i++) {
+      let testline = line + words[i] + SPACE;
+      let renderLength = ctx.measureText(testline);
+      if (renderLength.width > MAX_WIDTH && i > 0) {
+        if (this.d === 0)
+          console.info(`Kerning kernels ${i} at ${y}: ${testline}|${line}`);
+        ctx.fillText(line, x + PADDING_LEFT, y + PADDING_TOP);
+        line = words[i] + SPACE;
+        y += LINE_HEIGHT;
+      } else {
+        if (this.d === 0)
+          console.info(`line build ${i} at ${y}: ${testline}|${line}`);
+        line = testline;
+      }
+    }
+    ctx.fillText(line, x + PADDING_LEFT, y + PADDING_TOP);
+    this.d = 1;
   }
 }
