f29f74a382f377e604b1045d08e52081deb55941
Ryan Sweeney <sween119@hotmail.com>
2025-09-30 19:22:20 -0400

    add outdoor level boundaries

--- src/levels/OutdoorLevel.ts
+++ src/levels/OutdoorLevel.ts
@@ -40,26 +40,52 @@ export class OutdoorLevel extends Level {
   ready(): void {
     gameEvents.on(signals.sceneExit, this, () => {
       console.info(`Called from ${this.constructor.name} in Outdoorlevel`);
-      gameEvents.emit(signals.levelChange, new CaveLevel({ actorPosition: new Vector2(gridCells(2), gridCells(3)) }));
+      gameEvents.emit(
+        signals.levelChange,
+        new CaveLevel({
+          actorPosition: new Vector2(gridCells(2), gridCells(3)),
+        })
+      );
     });
   }
 
   defineWalls() {
-    // Define walls by their grid positions
-    this.walls.add(new Vector2(32, 32).toString()); // rock
-    this.walls.add(new Vector2(48, 32).toString());
-    this.walls.add(new Vector2(32, 48).toString());
-    this.walls.add(new Vector2(48, 48).toString());
-    this.walls.add(new Vector2(64, 48).toString()); // tree
+    this.walls.add(new Vector2(gridCells(12), gridCells(6)).toString()); // rock
+    this.walls.add(new Vector2(gridCells(13), gridCells(6)).toString());
+    this.walls.add(new Vector2(gridCells(14), gridCells(6)).toString());
 
-    this.walls.add(new Vector2(64, 64).toString()); // squares
-    this.walls.add(new Vector2(64, 80).toString());
-    this.walls.add(new Vector2(80, 64).toString());
-    this.walls.add(new Vector2(80, 80).toString());
+    this.walls.add(new Vector2(gridCells(4), gridCells(3)).toString()); // trees
+    this.walls.add(new Vector2(gridCells(14), gridCells(2)).toString());
+    this.walls.add(new Vector2(gridCells(13), gridCells(4)).toString());
 
-    this.walls.add(new Vector2(112, 80).toString()); // water
-    this.walls.add(new Vector2(128, 80).toString());
-    this.walls.add(new Vector2(144, 80).toString());
-    this.walls.add(new Vector2(160, 80).toString());
+    this.walls.add(new Vector2(gridCells(4), gridCells(4)).toString()); // squares
+    this.walls.add(new Vector2(gridCells(4), gridCells(5)).toString());
+    this.walls.add(new Vector2(gridCells(5), gridCells(4)).toString());
+    this.walls.add(new Vector2(gridCells(5), gridCells(5)).toString());
+    this.walls.add(new Vector2(gridCells(8), gridCells(3)).toString());
+    this.walls.add(new Vector2(gridCells(9), gridCells(3)).toString());
+
+    this.walls.add(new Vector2(gridCells(7), gridCells(5)).toString()); // water
+    this.walls.add(new Vector2(gridCells(8), gridCells(5)).toString());
+    this.walls.add(new Vector2(gridCells(9), gridCells(5)).toString());
+    this.walls.add(new Vector2(gridCells(10), gridCells(5)).toString());
+
+    this.walls.add(new Vector2(gridCells(14), gridCells(4)).toString()); // house
+
+    //edges
+    Array.from({ length: 4 }, (_, i) => i).forEach((y) => {
+      this.walls.add(new Vector2(gridCells(2), gridCells(y + 3)).toString());
+      this.walls.add(new Vector2(gridCells(16), gridCells(y + 3)).toString());
+    });
+
+    Array.from({ length: 13 }, (_, i) => i).forEach((x) => {
+      this.walls.add(new Vector2(gridCells(x + 3), gridCells(1)).toString());
+      this.walls.add(new Vector2(gridCells(x + 3), gridCells(7)).toString());
+    });
+
+    this.walls.add(new Vector2(gridCells(15), gridCells(2)).toString());
+    this.walls.add(new Vector2(gridCells(4), gridCells(2)).toString());
+    this.walls.add(new Vector2(gridCells(5), gridCells(2)).toString());
+    this.walls.add(new Vector2(gridCells(6), gridCells(2)).toString());
   }
 }
