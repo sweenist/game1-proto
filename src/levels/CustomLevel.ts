39911b17e15ae6622b9ab7fe77800ba53fdc8e6f
Ryan Sweeney <sween119@hotmail.com>
2025-10-13 22:44:53 -0400

    updated water tiles amd map

--- src/levels/CustomLevel.ts
+++ src/levels/CustomLevel.ts
@@ -13,39 +13,54 @@ import mapContent from './config/overworld.txt?raw';
 import levelConfig from './config/custom.config.json';
 
 type tempType = {
-  name: string;
+  resourceName: string;
   frameIndex: number;
 };
 
 export class CustomLevel extends Level {
-  mapAddresses: number[] = [];
-  fancyMap: tempType[] = [
-    { name: 'custom2', frameIndex: 0 },
-    { name: 'custom2', frameIndex: 1 },
-    { name: 'custom2', frameIndex: 2 },
-    { name: 'customOverworld', frameIndex: 3 },
-    { name: 'customOverworld', frameIndex: 4 },
-    { name: 'customOverworld', frameIndex: 5 },
-    { name: 'custom2', frameIndex: 9 },
-    { name: 'custom2', frameIndex: 4 },
-    { name: 'custom2', frameIndex: 8 },
-    { name: 'customOverworld', frameIndex: 9 },
-    { name: 'customOverworld', frameIndex: 10 },
-    { name: 'customOverworld', frameIndex: 11 },
-    { name: 'customOverworld', frameIndex: 12 },
-    { name: 'customOverworld', frameIndex: 13 },
-    { name: 'customOverworld', frameIndex: 14 },
-    { name: 'customOverworld', frameIndex: 15 },
-    { name: 'customOverworld', frameIndex: 16 },
-    { name: 'customOverworld', frameIndex: 17 },
-    { name: 'custom2', frameIndex: 6 },
-    { name: 'custom2', frameIndex: 3 },
-    { name: 'custom2', frameIndex: 10 },
-    { name: 'custom2', frameIndex: 2 },
-    { name: 'custom2', frameIndex: 11 },
-    { name: 'custom2', frameIndex: 12 },
-    { name: 'custom2', frameIndex: 12 },
-  ];
+  mapAddresses: string[] = [];
+  fancyMap: { [key: string]: tempType } = {
+    D1: { resourceName: 'custom2', frameIndex: 0 },
+    DN: { resourceName: 'custom2', frameIndex: 1 },
+    D2: { resourceName: 'custom2', frameIndex: 2 },
+    DX: { resourceName: 'custom2', frameIndex: 3 },
+    G1: { resourceName: 'custom2', frameIndex: 4 },
+    DW: { resourceName: 'custom2', frameIndex: 5 },
+    DC: { resourceName: 'custom2', frameIndex: 6 },
+    DE: { resourceName: 'custom2', frameIndex: 7 },
+    G0: { resourceName: 'custom2', frameIndex: 8 },
+    G2: { resourceName: 'custom2', frameIndex: 9 },
+    D3: { resourceName: 'custom2', frameIndex: 10 },
+    DS: { resourceName: 'custom2', frameIndex: 11 },
+    D4: { resourceName: 'custom2', frameIndex: 12 },
+    GR: { resourceName: 'custom2', frameIndex: 13 },
+    G3: { resourceName: 'custom2', frameIndex: 14 },
+    B1: { resourceName: 'custom2', frameIndex: 15 },
+    B2: { resourceName: 'custom2', frameIndex: 16 },
+    B5: { resourceName: 'custom2', frameIndex: 17 },
+    B6: { resourceName: 'custom2', frameIndex: 18 },
+    GD: { resourceName: 'custom2', frameIndex: 19 },
+    B3: { resourceName: 'custom2', frameIndex: 20 },
+    B4: { resourceName: 'custom2', frameIndex: 21 },
+    B7: { resourceName: 'custom2', frameIndex: 22 },
+    B8: { resourceName: 'custom2', frameIndex: 23 },
+    '12': { resourceName: 'customOverworld', frameIndex: 12 },
+    '13': { resourceName: 'customOverworld', frameIndex: 13 },
+    '14': { resourceName: 'customOverworld', frameIndex: 14 },
+    W1: { resourceName: 'custom3', frameIndex: 0 },
+    WN: { resourceName: 'custom3', frameIndex: 1 },
+    W2: { resourceName: 'custom3', frameIndex: 2 },
+    RV: { resourceName: 'custom3', frameIndex: 3 },
+    RH: { resourceName: 'custom3', frameIndex: 4 },
+    WW: { resourceName: 'custom3', frameIndex: 5 },
+    WC: { resourceName: 'custom3', frameIndex: 6 },
+    WE: { resourceName: 'custom3', frameIndex: 7 },
+    W5: { resourceName: 'custom3', frameIndex: 8 },
+    W6: { resourceName: 'custom3', frameIndex: 9 },
+    W3: { resourceName: 'custom3', frameIndex: 10 },
+    WS: { resourceName: 'custom3', frameIndex: 11 },
+    W4: { resourceName: 'custom3', frameIndex: 12 },
+  };
 
   constructor(params: LevelParams) {
     super({ actorPosition: new Vector2(gridCells(3), gridCells(8)) });
@@ -83,21 +98,16 @@ export class CustomLevel extends Level {
     const columns = lines[0].split(',').length;
     const rows = lines.length;
 
-    this.mapAddresses = lines
-      .flatMap((s) => s.split(','))
-      .map((value) => {
-        value.replace(/^0/, '');
-        return Number(value);
-      });
+    this.mapAddresses = lines.flatMap((s) => s.split(','));
 
     for (let y = 0; y < rows; y++) {
       for (let x = 0; x < columns; x++) {
         const mapTile = this.mapAddresses[y * columns + x];
         const tilecfg = this.fancyMap[mapTile];
-        const fc = config.find((c) => c.name === tilecfg.name);
+        const fc = config.find((c) => c.name === tilecfg.resourceName);
         if (!fc) continue;
         const tile = new Sprite({
-          resource: resources.images[tilecfg.name],
+          resource: resources.images[tilecfg.resourceName],
           frameColumns: fc?.frameConfig.columns,
           frameRows: fc?.frameConfig.rows,
           frameSize: Vector2.fromPoint(fc?.frameConfig.size),
